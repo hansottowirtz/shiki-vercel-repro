a023e441fad2583460541f6d73b6d0d27028deea
Hans Otto Wirtz <hansottowirtz@gmail.com>
2025-05-08 13:46:25 +0200

    add repro

---
+++ src/app/CodeToHtml.tsx
@@ -0,0 +1,25 @@
+import { makeSingletonHighlighter } from "shiki/bundle/web";
+import { createHighlighter } from "shiki";
+
+const getHighlighter = makeSingletonHighlighter(createHighlighter);
+
+export async function CodeToHtml({
+  code,
+  language,
+}: {
+  code: string;
+  language: string;
+}) {
+  
+  const highlighter = await getHighlighter({
+    themes: ['poimandres'],
+    langs: ["tsx"]
+  });
+
+  const html = await highlighter.codeToHtml(code, {
+    lang: language,
+    theme: "poimandres",
+  });
+
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+}
